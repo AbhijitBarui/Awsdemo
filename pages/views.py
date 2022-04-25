4caf3c2ee2bbb94278bbd942ab7e65dbf6231031
AbhijitBarui <abhijitongit@gmail.com>
2022-04-25 15:51:33 +0530

    template setup

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,25 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+#from listings.models import Listing
+#from realtors.models import Realtor
+#from listings.choices import state_choices, bedroom_choices, price_choices
 
 def index(request):
-    return HttpResponse('<h1>hello</h1>')
+#    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+#    context = {
+#        'listings': listings,
+#        'state_choices': state_choices,
+#        'bedroom_choices': bedroom_choices,
+#        'price_choices': price_choices,
+#    }
+#    return render(request, 'pages/index.html', context)
+    return render(request, 'pages/index.html')
+
+def about(request):
+    # realtors = Realtor.objects.order_by('-hire_date')
+    # mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+    # context = {
+    #     'realtors': realtors,
+    #     'mvp_realtors': mvp_realtors,
+    # }
+    # return render(request, 'pages/about.html', context)
+    return render(request, 'pages/about.html')
